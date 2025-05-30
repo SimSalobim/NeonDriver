c56c6be6e6d580dfe0cc6ce864de2f30acfb883c
SimSalobim <L0lol0lol0L@yandex.ru>
2025-05-31 02:20:20 +0300

    ПУПУПУ
    ❝ Чем ярче будет сиять его свет, тем мрачнее будут тени за его спиной. ❞

--- main/migrations/0002_car.py
+++ main/migrations/0002_car.py
@@ -3,13 +3,20 @@
 from django.db import migrations, models
 
 
-class Migration(migrations.Migration):
 
+def create_initial_cars(apps, schema_editor):
+    Car = apps.get_model('main', 'Car')
+    Car.objects.create(name="KUZANAGI CT-3X")
+    Car.objects.create(name="QUADRA TURBO-R V-TECH")
+
+
+class Migration(migrations.Migration):
     dependencies = [
         ('main', '0001_initial'),
     ]
 
     operations = [
+        migrations.RunPython(create_initial_cars),
         migrations.CreateModel(
             name='Car',
             fields=[
