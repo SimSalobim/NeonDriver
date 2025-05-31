8068b65c3c1c97e108c288fd00ef2219d5f98436
SimSalobim <L0lol0lol0L@yandex.ru>
2025-05-31 03:06:18 +0300

    Ласт трай и баиньки
    не, я этот веб

--- main/migrations/0002_add_initial_cars.py
+++ main/migrations/0002_add_initial_cars.py
@@ -7,11 +7,17 @@ def create_initial_cars(apps, schema_editor):
     Car.objects.create(name="QUADRA TURBO-R V-TECH")
 
 
+def reverse_initial_cars(apps, schema_editor):
+    Car = apps.get_model('main', 'Car')
+    Car.objects.filter(name="KUZANAGI CT-3X").delete()
+    Car.objects.filter(name="QUADRA TURBO-R V-TECH").delete()
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ('main', '0001_initial'),
     ]
 
     operations = [
-        migrations.RunPython(create_initial_cars),
+        migrations.RunPython(create_initial_cars, reverse_code=reverse_initial_cars),
     ]
