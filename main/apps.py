c20d83a665ea6b3815018f6599cbd74ae05777b7
SimSalobim <L0lol0lol0L@yandex.ru>
2025-06-05 02:32:40 +0300

    ашалет
    я паченел эта

--- main/apps.py
+++ main/apps.py
@@ -13,10 +13,6 @@ class MainConfig(AppConfig):
         from .models import Car
         from django.db.models.signals import post_migrate
 
-        def create_initial_data(sender, **kwargs):
             # Проверяем, что таблица существует
-            Car.objects.get_or_create(name="KUZANAGI CT-3X")
-            Car.objects.get_or_create(name="QUADRA TURBO-R V-TECH")
-
-        # Подключаем сигнал
-        post_migrate.connect(create_initial_data, sender=self)
+        Car.objects.get_or_create(name="KUZANAGI CT-3X")
+        Car.objects.get_or_create(name="QUADRA TURBO-R V-TECH")
