27b4d7bd6f05d398a5ae924b72055a8094398b14
SimSalobim <L0lol0lol0L@yandex.ru>
2025-06-06 18:44:54 +0300

    2 gjgsnrf dt, cjrtnjd
    сойдет

--- NeonDrive/settings.py
+++ NeonDrive/settings.py
@@ -56,15 +56,15 @@ LOGOUT_REDIRECT_URL = 'home'
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
     'channels',
-    'daphne',
+    'main',
 ]
 
 MIDDLEWARE = [
