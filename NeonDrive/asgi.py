62522401603e84475f467275afaa8f967e8fcc21
SimSalobim <L0lol0lol0L@yandex.ru>
2025-06-07 12:41:27 +0300

    ввввввввв

--- NeonDrive/asgi.py
+++ NeonDrive/asgi.py
@@ -1,22 +1,21 @@
 import os
+import django  # Добавьте этот импорт
 from django.core.asgi import get_asgi_application
-from django.template.backends import django
 
 # Важно: сначала установить переменные окружения
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NeonDrive.settings')
+django.setup()  # Теперь это будет работать корректно
 
 # Затем импортировать остальные модули
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import main.routing
 
-# Убедитесь, что приложение инициализируется после настроек
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NeonDrive.settings')
-django.setup()  # Добавьте эту строку
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(main.routing.websocket_urlpatterns)
+        URLRouter(
+            main.routing.websocket_urlpatterns
+        )
     ),
 })
